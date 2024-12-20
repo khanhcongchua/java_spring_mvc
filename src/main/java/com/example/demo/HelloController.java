fab753c2025805af491535a4dbc362a6291937ad
Khanh Bo <nduykhan650@gmail.com>
2024-12-21 03:45:10 +0700

    hello world

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from Khanh Cong Chua!";
+    }
+    
+    
+}
