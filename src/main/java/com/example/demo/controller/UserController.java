988243857d684f10bce0020694bda37c22d3139b
Khanh Bo <nduykhan650@gmail.com>
2024-12-23 15:18:22 +0700

    them form ....them attribute post lay data tu view .....path => xac dinh thuoc tinh cua User.....form:form de javaspring nhan du lieu tu form.....them modelAttribute=newUser la key => ben controller co the biet la du lieu moi....

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 
 
@@ -18,8 +22,23 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage(){
+    public String getHomePage(Model model){
         String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from with model");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model){
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit){
+        System.out.println("run here!" + hoidanit);
         return "hello";
     }
 }
