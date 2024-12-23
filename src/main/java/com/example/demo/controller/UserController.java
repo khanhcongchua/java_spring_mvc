018da699e8bd8cea42c2d4e80bc979612c4a8481
Khanh Bo <nduykhan650@gmail.com>
2024-12-23 18:48:10 +0700

    ham lay tat ca user bang https://docs.spring.io/spring-data/jpa/reference/repositories/query-keywords-reference.html

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,8 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model){
+        List<User> arrUsers = this.userService.getAllUsersByEmail("nduykhanh650@gmail.com");
+        System.out.println(arrUsers);
         model.addAttribute("eric", "test");
         model.addAttribute("hoidanit", "from with model");
         return "hello";
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
@@ -17,8 +19,13 @@ public class UserService {
 
 
 
-    public String handleHello(){
-        return "Hello from service";
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
+    }
+
+
+    public List<User> getAllUsersByEmail(String email){
+        return this.userRepository.findByEmail(email);
     }
 
 
