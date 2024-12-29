017b411823f42f76e447e7e3ac33bcde71841cea
Khanh Bo <nduykhan650@gmail.com>
2024-12-29 17:49:53 +0700

    them fake data hinh anh

--- src/main/java/com/example/demo/controller/client/HomePageController.java
+++ src/main/java/com/example/demo/controller/client/HomePageController.java
@@ -1,15 +1,32 @@
 package com.example.demo.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.domain.Product;
+import com.example.demo.service.ProductService;
 
 @Controller
 public class HomePageController {
+    private ProductService productService;
+
+    public HomePageController(ProductService productService){
+        this.productService = productService;
+    }
     
 
 
     @GetMapping("/")
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        List<Product> flowers = this.productService.getAllProducts();
+        model.addAttribute("flowers1", flowers);//(gia tri nhan duoc ben  view, gia tri ben controller)
         return "client/homepage/show";
     }
+
+
+   
 }
